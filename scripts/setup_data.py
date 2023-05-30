3a8c6e74bb53971f2404d10b043e2ae8f1c7da14
KTFish <90987511+KTFish@users.noreply.github.com>
2023-05-30 08:45:49 +0200

    Implementing dataloaders

---
+++ scripts/setup_data.py
@@ -0,0 +1,36 @@
+import torch
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader
+
+
+def create_datasets(train_dir, test_dir, transform=torch.transforms.ToTensor()):
+    # Create MNIST Train and Test Datasets
+    train_dataset = MNIST(
+        root=train_dir,
+        train=True,
+        download=True,
+        transform=transform,
+        target_transform=None,
+    )
+
+    test_dataset = MNIST(
+        root=test_dir,
+        train=False,
+        download=True,
+        transform=transform,
+        target_transform=None,
+    )
+
+    return train_dataset, test_dataset, train_dataset.classes
+
+
+def create_dataloaders(train_dir, test_dir, batch_size=32):
+    # Create datasets and get class names
+    train_dataset, test_dataset, class_names = create_datasets(train_dir, test_dir)
+
+    # Turn datasets into dataloaders
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_dataloader, test_dataloader, class_names
