513dfc5078500c15f719844b86169ddc2324c1f6
KTFish <90987511+KTFish@users.noreply.github.com>
2023-06-19 07:29:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,22 +12,11 @@ HIDDEN_UNITS = 10
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
-# Paths
-train_dir = r"C:\Users\janek\notebooks\0_Projects\KAGGLE\data\mnist\train"
-test_dir = r"C:\Users\janek\notebooks\0_Projects\KAGGLE\data\mnist\test"
-
-# Create folders if needed
-if not os.path.exists(train_dir):
-    os.makedirs(train_dir, exist_ok=True)
-
-if not os.path.exists(test_dir):
-    os.makedirs(test_dir, exist_ok=True)
-
-
 # Create dataloaders using `setup_data.py` script
 train_dataloader, test_dataloader, class_names = setup_data.create_dataloaders(
-    train_dir=train_dir, test_dir=test_dir, batch_size=BATCH_SIZE
+    batch_size=BATCH_SIZE
 )
+
 
 # Plot a random sample from the dataset
 utils.visualize_sample_image(class_names=class_names, dataloader=train_dataloader)
@@ -36,7 +25,9 @@ assert len(class_names) == 10  # MNIST has 10 classes (Digits 0 - 9)
 
 
 ### Setup a Model
-model = model_spawner.VGG(input=1, output=len(class_names), hidden_units=15).to(device)
+model = model_spawner.VGG(
+    input=1, output=len(class_names), hidden_units=HIDDEN_UNITS
+).to(device)
 # model = model_spawner.BaseLine(
 #     input=28 * 28, output=len(class_names), hidden_units=HIDDEN_UNITS
 # ).to(device)
